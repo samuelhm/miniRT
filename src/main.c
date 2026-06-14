samuelhm/miniRT

commit fc303e8e43d056b25302f6ce9107f89eaf36a3fd

feat: default render mode is now ONE (was FAST)
Program now starts with render_one (sharp, no jitter, full path trace)
instead of render_fast (low-res preview + console UI).
Console mode still accessible by pressing 'C'.

src/main.c
@@ -48,7 +48,7 @@ void	mouse_click(mouse_key_t button, action_t action, \
 	if (action == 1)
 	{
 		if (data->last_render != FAST)
-			data->render_sel = render_fast;
+	data->render_sel = render_one;
 		mlx_get_mouse_pos(data->mlx, &x, &y);
 		console_click(data, x, y);
 	}

commit 3871d4121f3580864731751e081b8788c046904a

fix: actual default render mode ONE (was editing wrong line)
The previous commit fc303e8 accidentally changed mouse_click() instead
of main(). This commit:
- Changes main.c:69 from render_fast to render_one (the ACTUAL default)
- Reverts mouse_click back to render_fast (click should open console)
- Removes unnecessary console background cleanup from render_one
- Fixes broken indentation in mouse_click

src/main.c
@@ -48,7 +48,7 @@ void	mouse_click(mouse_key_t button, action_t action, \
 	if (action == 1)
 	{
 		if (data->last_render != FAST)
-	data->render_sel = render_one;
+			data->render_sel = render_fast;
 		mlx_get_mouse_pos(data->mlx, &x, &y);
 		console_click(data, x, y);
 	}
@@ -66,7 +66,7 @@ int	main(int ac, char **av)
 	close(fd);
 	init_mlx(data);
 	init_all(data);
-	data->render_sel = render_fast;
+	data->render_sel = render_one;
 	mlx_resize_hook(data->mlx, &resise_w, data);
 	mlx_loop_hook(data->mlx, call_render, data);
 	mlx_mouse_hook(data->mlx, mouse_click, data);

src/render/render/three_renders.c
@@ -21,11 +21,6 @@ void	render_one(void *param)
 	data->x = data->mlx->width;
 	data->y = data->mlx->height;
 	data->god = true;
-	if (data->console->background)
-	{
-		mlx_delete_image(data->mlx, data->console->background);
-		data->console->background = NULL;
-	}
 	mlx_resize_image(data->img, (uint32_t)data->x, (uint32_t)data->y);
 	new_img = render(data, 1);
 	fill_image(data, (uint32_t *)data->img->pixels, new_img);